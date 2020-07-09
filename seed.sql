f70ec11f0d60272475e5cb7415eeae7ed0907014
Ehson Raza <ehson.raza1@outlook.com>
2020-07-08 20:12:23 -0500

    Cleaning up SQL

--- seed.sql
+++ seed.sql
@@ -1,28 +1,32 @@
-DROP DATABASE IF EXISTS employee_DB;
-CREATE database employee_DB;
+CREATE DATABASE IF NOT EXISTS employee_DB;
+
 
 USE employee_DB;
 
+CREATE TABLE department (
+ id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10) NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(100) NULL,
   last_name VARCHAR(100) NULL,
   role_id INT,
   manager_id INT,
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
-CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30) NULL,
-  salary DECIMAL,
-  department_id INT,
-  PRIMARY KEY (id)
-);
-CREATE TABLE department (
- id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NULL,
-  PRIMARY KEY (id)
-);
+
 
 INSERT into employee(first_name, last_name, role_id, manager_id) VALUES("Bob", "Smith", 1, 1);
 INSERT into employee(first_name, last_name, role_id, manager_id) VALUES("Billy", "Schmith", 2, 2);
