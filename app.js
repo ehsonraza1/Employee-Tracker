c61c33b44cac042cfd6261cdbdfb99b24a1af24c
Ehson Raza <ehson.raza1@outlook.com>
2020-07-08 23:00:55 -0500

    Updated ReadMe, File Reorg and app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql2");
 var inquirer = require("inquirer");
+var console = require("console.table");
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
@@ -16,6 +17,72 @@ var connection = mysql.createConnection({
   database: "employee_DB",
 });
 
+figlet("Employee Manager", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
+
+//TODO
+// 3 Functions create Department, Employee, and Role - CRUD Manipulation
+//Differentiate b/w functions that are manipulating the DB and
+//functions collecting data from inquirer
+
+inquirer
+  .prompt({
+    name: "action",
+    type: "list",
+    message: "What would you like to review?",
+    choices: [
+      "View All Company Employees",
+      "View Employees By Role",
+      "View Employees By Department",
+      "Add Company Employee",
+      "Add Company Role",
+      "Add Company Department",
+      "Update Employees Role",
+      "Exit Application",
+    ],
+  })
+  .then(function (answer) {
+    switch (answer.action) {
+      case "View All Company Employees":
+        viewAllEmployees();
+        break;
+
+      case "View Employees By Role":
+        viewEmployeeRole();
+        break;
+
+      case "View Employees By Department":
+        viewEmployeeDept();
+        break;
+
+      case "Add Company Employee":
+        addEmployee();
+        break;
+
+      case "Add Company Role":
+        addRole();
+        break;
+
+      case "Add Company Department":
+        addDept();
+        break;
+
+      case "Update Employee Role":
+        updateEmployeeRole();
+        break;
+
+      case "Exit":
+        connection.end();
+        break;
+    }
+  });
+/*
 // connect to the mysql server and sql database
 connection.connect(function (err) {
   if (err) throw err;
@@ -48,3 +115,4 @@ function createDepartment(name) {
     start();
   });
 }
+*/
